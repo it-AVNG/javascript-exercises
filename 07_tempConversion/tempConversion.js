e83dc6c58483a2d6796a709ff05fc9e9af526496
it-AVNG <it.avng@gmail.com>
2022-11-16 12:08:28 +0700

    Add solution to Excercise 07, all test passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const ftoc = function() {
+const ftoc = function(t1) {
+  let celsius = (t1 - 32) * 5/9;
 
+  return Number(Math.round(celsius+'e1')+'e-1');
 };
 
-const ctof = function() {
+const ctof = function(t2) {
+  let Fahrenheit = (t2 * 9 /5) + 32;
+  
+  return Number(Math.round(Fahrenheit+'e1')+'e-1'); 
 
 };
 
