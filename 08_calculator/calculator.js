7c8bf990394a4a5e47ab9f44e0e0f42db295d398
it-AVNG <it.avng@gmail.com>
2022-12-21 22:51:26 +0700

    finish excercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let total = 0;
+  for (let i=0;i<a.length;i++){
+  total = add(total,a[i])}
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  let product = 1;
+  for (let i=0;i<a.length;i++){
+  product = product * a[i]}
 
+  return product;
+  
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let power =1;
+  for (let i = 1; i<=3; i++){
+  power = power * a;}
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let pound = 1;
+  for (let i=1; i<=a;i++){
+    pound = pound *i;
+  }
+  return pound
 };
 
 // Do not edit below this line
