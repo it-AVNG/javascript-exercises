5b88eec7358d3c2706a65b2ca758fca6ffb72f68
it-AVNG <it.avng@gmail.com>
2022-12-25 07:54:31 +0700

    finish excercise using array.map() method

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,28 @@
-const getTheTitles = function() {
+//const debug = require("debug")("title");
+//log into cmd DEBUG=title node getTheTitles.js
+function mapTitle(item){
+  return item.title;
+}
 
+const getTheTitles = function(objectArray) {
+  return objectArray.map(mapTitle);
 };
+
+//for debuging;
+// const books = [
+//   {
+//     title: 'Book',
+//     author: 'Name'
+//   },
+//   {
+//     title: 'Book2',
+//     author: 'Name2'
+//   }
+// ]
+
+
+// const namme =getTheTitles(books);
+// debug(namme);
 
 // Do not edit below this line
 module.exports = getTheTitles;
